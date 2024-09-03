0d78bef555b1dd01620c4cf4e7ea2b65d63e2d8e
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-03 17:23:19 +0200

    introducing the our products session

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import './Home.css'
 import hero from "../Assets/download__14_-removebg-preview.png"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
   return (
     <>
       <section>
-        <div>
-          <div>
+        <div className='hero'>
+          <div className='hero_img'>
             <img src={hero} alt='' />
           </div>
           <div>
@@ -16,8 +18,36 @@ const Home = () => {
         </div>
       </section>
       <section>
-        <div>
-
+        <div className='container'>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card middle-card">
+                <img src='' className="card-img-top" alt="Side Image 1" />
+                <div className="card-body">
+                  <h5 className="card-title">Image 1</h5>
+                  <p className="card-text">Description of the first image.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card middle-card">
+                <img src='' className="card-img-top" alt="Middle Image" />
+                <div className="card-body">
+                  <h5 className="card-title">Image 2</h5>
+                  <p className="card-text">Description of the middle image.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card middle-card">
+                <img src='' className="card-img-top" alt="Side Image 2" />
+                <div className="card-body">
+                  <h5 className="card-title">Image 3</h5>
+                  <p className="card-text">Description of the third image.</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
