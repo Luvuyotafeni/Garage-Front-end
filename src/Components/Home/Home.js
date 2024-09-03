d08190ade67ceb4a75aa30b6999ddbf8edb759d4
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-03 15:01:50 +0200

    hero section on the home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import hero from "../Assets/download__14_-removebg-preview.png"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section>
+        <div>
+          <div>
+            <img src={hero} alt='' />
+          </div>
+          <div>
+            <h3>Lastro Workshop</h3>
+            <p>if we can't fix i tno one can <span>Period</span></p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+
+        </div>
+      </section>
+    </>
   )
 }
 
