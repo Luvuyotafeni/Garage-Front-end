c77b09e8f58f8e25f779c968393e14cc7378f0f4
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-04 10:14:44 +0200

    decreasing the size of images

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,6 +7,7 @@ import engine from '../Assets/transport_14982232.png'
 import manual from '../Assets/manual-transmission_2557346.png'
 import starter from '../Assets/starter_6509098.png'
 import oil from '../Assets/engine-oil_1820651.png'
+import brakes from '../Assets/car_12841175.png'
 
 const Home = () => {
   return (
@@ -25,59 +26,77 @@ const Home = () => {
       <section>
         <div className='container'>
           <div className="row">
-            <div className="col-md-4 mb-2 mt-2">
+            <div className="col-md-3 mb-2 mt-2">
               <div className="card middle-card">
-                <img src={engine} className="card-img-top" alt="Side Image 1" />
+                <img src={engine} className="card-img-top img-fluid w-25 mx-auto" alt="Side Image 1" />
                 <div className="card-body">
                   <h5 className="card-title">Image 1</h5>
                 </div>
               </div>
             </div>
-            <div className="col-md-4 mb-2 mt-2">
+            <div className="col-md-3 mb-2 mt-2">
               <div className="card middle-card">
-                <img src='' className="card-img-top" alt="Middle Image" />
+                <img src={brakes} className="card-img-top img-fluid w-25 mx-auto" alt="Middle Image" />
                 <div className="card-body">
                   <h5 className="card-title">Image 2</h5>
                 </div>
               </div>
             </div>
-            <div className="col-md-4 mb-2 mt-2">
+            <div className="col-md-3 mb-2 mt-2">
               <div className="card middle-card">
-                <img src={ai} className="card-img-top" alt="Side Image 2" />
+                <img src={ai} className="card-img-top img-fluid w-25 mx-auto" alt="Side Image 2" />
                 <div className="card-body">
                   <h5 className="card-title">Image 3</h5>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3 mb-2 mt-2">
+              <div className="card middle-card">
+                <img src={manual} className="card-img-top img-fluid w-25 mx-auto" alt="Side Image 1" />
+                <div className="card-body">
+                  <h5 className="card-title">Image 4</h5>
                 </div>
               </div>
             </div>
           </div>
           <div className="row">
-            <div className="col-md-4 mb-2 mt-2">
+            <div className="col-md-3 mb-2 mt-2">
               <div className="card middle-card">
-                <img src={manual} className="card-img-top" alt="Side Image 1" />
+                <img src={starter} className="card-img-top img-fluid w-25 mx-auto" alt="Middle Image" />
                 <div className="card-body">
-                  <h5 className="card-title">Image 1</h5>
+                  <h5 className="card-title">Image 5</h5>
                 </div>
               </div>
             </div>
-            <div className="col-md-4 mb-2 mt-2">
+            <div className="col-md-3 mb-2 mt-2">
               <div className="card middle-card">
-                <img src={starter} className="card-img-top" alt="Middle Image" />
+                <img src={oil} className="card-img-top img-fluid w-25 mx-auto" alt="Side Image 2" />
                 <div className="card-body">
-                  <h5 className="card-title">Image 2</h5>
+                  <h5 className="card-title">Image 6</h5>
                 </div>
               </div>
             </div>
-            <div className="col-md-4 mb-2 mt-2">
+            <div className="col-md-3 mb-2 mt-2">
               <div className="card middle-card">
-                <img src={oil} className="card-img-top" alt="Side Image 2" />
+                <img src={oil} className="card-img-top img-fluid w-25 mx-auto" alt="Side Image 2" />
                 <div className="card-body">
-                  <h5 className="card-title">Image 3</h5>
+                  <h5 className="card-title">Image 6</h5>
                 </div>
               </div>
             </div>
+            <div className="col-md-3 mb-2 mt-2">
+              <div className="card middle-card">
+                <img src={oil} className="card-img-top img-fluid w-25 mx-auto" alt="Side Image 2" />
+                <div className="card-body">
+                  <h5 className="card-title">Image 6</h5>
+                </div>
+              </div>
+            </div>
+            
           </div>
         </div>
       </section>
+
     </>
   )
 }
