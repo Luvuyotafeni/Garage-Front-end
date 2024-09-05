1fbd35d23ea196cfc125e685c417305c28ada921
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-05 14:12:22 +0200

    added the fototer elements

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            <h3>Quick Links</h3>
+            <div></div>
+        </div>
+        <div>
+            <h3>Address</h3>
+            <div>
+                <p></p>
+            </div>
+        </div>
+        <div>
+            <h3>Working Hours</h3>
+            <div>
+                <p>Mon-fri <span></span></p>
+                <p>Mon-fri <span></span></p>
+                <p>Mon-fri <span></span></p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
