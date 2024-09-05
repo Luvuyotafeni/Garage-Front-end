43f3c2de6b1eaf87fa4f90bbde66dda829f6810d
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-05 14:18:50 +0200

    added the footer.css codes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='shorts'>
         <div>
             <h3>Quick Links</h3>
             <div></div>
@@ -10,15 +11,19 @@ const Footer = () => {
         <div>
             <h3>Address</h3>
             <div>
-                <p></p>
+                <p>
+                    SHOP NO: 10, EZIMBUZINI AUTO HUB, ISIZINDA
+                    SOMAKHENIKHA,MUNICIPALITY MECHANICAL WORKSHOPS, 
+                    CORNER OF PRINCE MCWAYIZENI DRIVE, DURBAN 4089
+                </p>
             </div>
         </div>
         <div>
             <h3>Working Hours</h3>
             <div>
-                <p>Mon-fri <span></span></p>
-                <p>Mon-fri <span></span></p>
-                <p>Mon-fri <span></span></p>
+                <p>Mon-fri <span>07:00 - 17:00</span></p>
+                <p>Sat-Sun <span>08:00 - 15:00</span></p>
+                <p>Public holidays<span>09:00 - 14:00</span></p>
             </div>
         </div>
     </div>
