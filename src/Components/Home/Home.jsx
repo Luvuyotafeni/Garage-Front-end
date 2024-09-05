b1d8d2290007b0d24efa101ed4f4eb1b6d5460e6
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-05 12:48:28 +0200

    editing the testimonials

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -148,19 +148,19 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section>
+      <section className='mt-2 mb-2'>
         <h2 className='text-center' >Team</h2>
-        <div className='row'>
+        <div className='row employee_box'>
           <div className='col-md-4'>
             <div className='card middle-card side'>
-              <img src={worker1} />
+              <img src={worker1} className="card-img-top img-fluid w-75 mx-auto" />
               <div className='card-body'></div>
               <h5 className='card-title text-center'>Lawrence Mthembu</h5>
             </div> 
           </div>
           <div className='col-md-4'>
             <div className='card '>
-            <img src={worker2} />
+            <img src={worker2} className="card-img-top img-fluid w-75 mx-auto" />
             <div className='card-body'>
               <h5 className='card-title text-center'>Mary Sobukhwe</h5>
 
@@ -169,7 +169,7 @@ const Home = () => {
           </div>
           <div className='col-md-4'>
             <div className='card middle-card side'>
-              <img src={worker3} />
+              <img src={worker3} className="card-img-top img-fluid w-75 mx-auto"/>
               <div className='card-body' >
                 <h5 className='card-title text-center'>Zukile Msamo</h5>
 
@@ -179,7 +179,7 @@ const Home = () => {
         </div>
       </section>
       <section>
-          <div className='container'>
+          <div className='container '>
             <div className='row'>
               <div className='col-md-4'>
                 <div className='card middle-card side'>
@@ -209,8 +209,55 @@ const Home = () => {
           </div>
       </section>
       <section>
-        
+        <h2 className='text-center mt-2'>Testimonials</h2>
+        <div className='d-flex justify-content-center'>
+          <div id="carouselExampleIndicators" className="carousel slide w-50" data-bs-ride="carousel">
+            <ol className="carousel-indicators">
+              <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"></li>
+              <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+              <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+            </ol>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <div className="d-block w-100">
+                  <p className='text-center'>
+                    The most incredibly helpful team. They came to our rescue,
+                    made sure we were safe and sorted out our car in a few hours.
+                    Thank you a million times. We are so grateful.
+                  </p>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <div className="d-block w-100">
+                  <p className='text-center'>
+                    Thandeka Ngcobo<br />
+                    Upvotes on their friendly driver that dropped me off for free (as well as picked up the car),
+                    and also they cleaned my car inside and out before returning it. Almost a bad experience
+                    with them as a whole, but they recovered.
+                  </p>
+                </div>
+              </div>
+              <div className="carousel-item">
+                <div className="d-block w-100">
+                  <p className='text-center'>
+                    My car Hyundai I20 had a water pump problem, it was overheating and the starter was damaged.
+                    It was fixed in 3 hours, up and running till today. I will recommend LASTROALPICINOS WORKSHOP Mechanic.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </a>
+          </div>
+        </div>
       </section>
+
 
     </>
   )
