7a751a2d549c6399f76aca1f16d25b039651fbfa
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-06 16:41:50 +0200

    exported card.js

--- src/Components/Gallery/card.js
+++ src/Components/Gallery/card.js
@@ -1,78 +1,80 @@
-import img1 from './Assets/1.jpg'
-import img2 from './Assets/2.jpg'
-import img3 from './Assets/3.jpg'
-import img4 from './Assets/4.jpg'
-import img5 from './Assets/5.jpg'
-import img6 from './Assets/6.jpg'
-import img7 from './Assets/7.jpg'
-import img8 from './Assets/8.jpg'
-import img9 from './Assets/9.jpg'
-import img10 from './Assets/10.jpg'
-import img11 from './Assets/11.jpg'
-import img12 from './Assets/12.jpg'
-import img13 from './Assets/13.jpg'
-import img14 from './Assets/14.jpg'
-import img15 from './Assets/15.jpg'
+import img1 from './Assets/1.jpg';
+import img2 from './Assets/2.jpg';
+import img3 from './Assets/3.jpg';
+import img4 from './Assets/4.jpg';
+import img5 from './Assets/5.jpg';
+import img6 from './Assets/6.jpg';
+import img7 from './Assets/7.jpg';
+import img8 from './Assets/8.jpg';
+import img9 from './Assets/9.jpg';
+import img10 from './Assets/10.jpg';
+import img11 from './Assets/11.jpg';
+import img12 from './Assets/12.jpg';
+import img13 from './Assets/13.jpg';
+import img14 from './Assets/14.jpg';
+import img15 from './Assets/15.jpg';
 
-const itemData = [
-    {
-      img: img1,
-      title: 'Bed',
-    },
-    {
-      img: img2,
-      title: 'Books',
-    },
-    {
-      img: img3,
-      title: 'Sink',
-    },
-    {
-      img: img4,
-      title: 'Kitchen',
-    },
-    {
-      img: img5,
-      title: 'Blinds',
-    },
-    {
-      img: img6,
-      title: 'Chairs',
-    },
-    {
-      img: img7,
-      title: 'Laptop',
-    },
-    {
-      img: img8,
-      title: 'Doors',
-    },
-    {
-      img: img9,
-      title: 'Coffee',
-    },
-    {
-      img: img10,
-      title: 'Storage',
-    },
-    {
-      img: img11,
-      title: 'Candle',
-    },
-    {
-      img: img12,
-      title: 'Coffee table',
-    },
-    {
-      img: img13,
-      title: 'Coffee table',
-    },
-    {
-      img: img14,
-      title: 'Coffee table',
-    },
-    {
-      img: img15,
-      title: 'Coffee table',
-    },
-  ];
+export const itemData = [
+  {
+    img: img1,
+    title: 'Bed',
+  },
+  {
+    img: img2,
+    title: 'Books',
+  },
+  {
+    img: img3,
+    title: 'Sink',
+  },
+  {
+    img: img4,
+    title: 'Kitchen',
+  },
+  {
+    img: img5,
+    title: 'Blinds',
+  },
+  {
+    img: img6,
+    title: 'Chairs',
+  },
+  {
+    img: img7,
+    title: 'Laptop',
+  },
+  {
+    img: img8,
+    title: 'Doors',
+  },
+  {
+    img: img9,
+    title: 'Coffee',
+  },
+  {
+    img: img10,
+    title: 'Storage',
+  },
+  {
+    img: img11,
+    title: 'Candle',
+  },
+  {
+    img: img12,
+    title: 'Coffee table',
+  },
+  {
+    img: img13,
+    title: 'Coffee table',
+  },
+  {
+    img: img14,
+    title: 'Coffee table',
+  },
+  {
+    img: img15,
+    title: 'Coffee table',
+  },
+];
+
+export default itemData;
