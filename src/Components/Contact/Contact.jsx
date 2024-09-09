44eaf83d42646e7077e8801b24fc3a98f1c0e7b4
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-09 16:40:28 +0200

    added the contact form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,9 +5,9 @@ import contact_img from '../Assets/images (5).jpg'
 const Contact = () => {
   return (
     <div>
-      <h2>Contact Us</h2>
+      <h2 className='text-center'>Contact Us</h2>
       <div className='contact_img mt-2 mb-2'>
-        <img src={contact_img}/>
+        <img src={contact_img} alt='comntact_img' className='contact_img'/>
       </div>
       <div className='container mt-2'>
         <div className='row'>
@@ -34,6 +34,25 @@ const Contact = () => {
           </div>
         </div>
         
+      </div>
+      <div className='container'>
+        <form>
+          <div className='form-group mt-2 mb-2'>
+            <label>Name</label>
+            <input></input>
+          </div>
+          <div className='form-group mb-2 mt-2'>
+            <label>Email</label>
+            <input></input>
+          </div>
+          <div className='form-group mb-2 mt-2'>
+            <label>Subject</label>
+            <input></input>
+          </div>
+          <div>
+            <textarea rows={10} cols={60}></textarea>
+          </div>
+        </form>
       </div>
     </div>
   )
