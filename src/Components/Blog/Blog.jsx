bbfc97d53de5305a7026c6be8e82b70d0390c84f
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-09 10:12:58 +0200

    added blog information

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,7 +2,55 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div>
+      <h2>Blog</h2>
+      <div>
+        <h3>WHY TURBOCHARGER FAILS</h3>
+        <ul>
+          
+          <li>Most failures are caused by the three 'turbo killers' of oil starvation, 
+            oil contamination and foreign object damage. More than 90% of turbocharger 
+            failures are caused oil related either by oil starvation or oil contamination. 
+            Blocked or leaking pipes or lack of priming on fitting usually causes oil starvation.
+          </li>
+        </ul>
+        <h3>SIGN FOR BAD OXYGEN SENSOR </h3>
+        <ul>         
+          <li>While the damage may seem small initially, the longer you drive with a bad oxygen sensor
+            , the worse the damage will become. Eventually, you may experience rough idling, poor acceleration,
+             engine misfires, an illuminated check engine light, and failed emission tests.
+          </li>
+        </ul>
+        <h3>SIGNS FOR BADCAMSHAFT SEALS</h3>
+        <ul>
+          <li>Oil Leak</li>
+          <li>Smoke from Engine Bay</li>
+        </ul>
+        <h3>OIL PUMP FAILURE CAUSES </h3>
+        <ul>
+          <li>
+          Low Engine Oil Levels. If you don't have enough oil in your engine, you may have a problem. ...</li>
+          <li>Infrequent Oil Changes. ...</li>
+          <li>Wear and Tear Over Time. ...</li>
+          <li>Engine Sludge. ...</li>
+          <li>Oil Contamination. ...</li>
+          <li>Open Bypass Valves. ...</li>
+          <li>Malfunctioning Oil Passages or Oil Lines. ...</li>
+          <li>Restricting the Oil Suction Tube.</li>
+        </ul>
+        <h3>WHEN TO CHANGE YOUR FLY WHEEL </h3>
+        <ul>
+          <li>If your flywheel is making an unusual noise, this indicates it has become severely worn and should 
+            be replaced as soon as possible. Flywheels usually last around 80,000 to 100,000 miles, if not more, 
+            but as soon as you notice symptoms of an underlying problem, you should have your car checked by a mechanic.</li>
+        </ul>
+        <h3>BAD PILOT BEARING</h3>
+        <ul>
+          <li>A whirring sound when the clutch is engaged may indicate a failing pilot bearing. If the noise stops
+             when you depress the pedal and starts when you release the pedal, the pilot bearing is likely at fault.</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
