136c3f301a730984ef538306a61ae7623c525192
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-09 14:08:45 +0200

    added the contact image, created few cards in the contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import './Contact.css'
+import contact_img from '../Assets/images (5).jpg'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <h2>Contact Us</h2>
+      <div className='contact_img mt-2 mb-2'>
+        <img src={contact_img}/>
+      </div>
+      <div className='container mt-2'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <div className='card-body'>
+                <h5>Receptionist</h5>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+            <div className='card-body'>
+                <h5>Salesman</h5>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+            <div className='card-body'>
+                <h5>Salesman</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
