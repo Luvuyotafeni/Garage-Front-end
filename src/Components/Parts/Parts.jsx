2f6b2cd6a7a316834a089690f8404c8f328010e8
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-09 11:06:44 +0200

    full parts page

--- src/Components/Parts/Parts.jsx
+++ src/Components/Parts/Parts.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import './Parts.css'
 
 const Parts = () => {
   return (
-    <div>Parts</div>
+    <div>
+      <h2 className='text-center mt-2'>Parts</h2>
+      <div className='blog'>
+        <h5>ENGINE PARTS</h5>
+        <ul>
+          <li>RINGS AND BEARINGS</li>
+          <li>CONRODS</li>
+          <li>PISTOLS</li>
+          <li>SLEEVES</li>
+          <li>CRANKSHAFTS</li>
+          <li>COMPLETE CYLINDER HEAD</li>
+          <li>ENGINE SUB UNIT</li>
+          <li>COMPLETE ENGINES</li>
+          <li>CAMSHAFT</li>
+          <li>VALVES</li>
+          <li>VALVE GAURDS</li>
+          <li>VALVE STEM SEALS</li>
+          <li>MAIN SEAL</li>
+          <li>FRONT SEAL</li>
+          <li>SENSORS</li>
+        </ul>
+        <h5>OTHER PARTS</h5>
+        <ul>
+          <li>CLUTCH KIT</li>
+          <li>FLY WHEEL</li>
+          <li>GEARBOX COMPLETE OR EXCHANGE</li>
+          <li>DIFFERENTIAL EXCHANGE</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
