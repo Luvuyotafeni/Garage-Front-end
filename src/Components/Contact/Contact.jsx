e1aa6abd1ebad076bddd7ecdabf9b17c3f21f9c3
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-10 15:16:48 +0200

    form styled in the center of the page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -9,65 +9,72 @@ const Contact = () => {
       <div className='container contact_img mt-2 mb-2'>
         <img src={contact_img} alt='contact_img' className='contact_img center-text'/>
       </div>
+
       <div className='container mt-2'>
         <div className='row'>
           <div className='col-md-4'>
             <div className='card middle-card'>
               <div className='card-body'>
                 <h5 className='text-center'>Receptionist</h5>
-                <p><i class='bx bxs-user'></i>Qiqa Mbika</p>
-                <p><i class='bx bxl-whatsapp'></i>0839253459</p>
+                <p><i className='bx bxs-user'></i> Qiqa Mbika</p>
+                <p><i className='bx bxl-whatsapp'></i> 0839253459</p>
               </div>
             </div>
           </div>
           <div className='col-md-4'>
             <div className='card middle-card'>
-            <div className='card-body'>
+              <div className='card-body'>
                 <h5 className='text-center'>Salesman</h5>
-                <p><i class='bx bxs-user'></i>Calvin Moss</p>
-                <p><i class='bx bxl-whatsapp'></i>0732963634</p>
+                <p><i className='bx bxs-user'></i> Calvin Moss</p>
+                <p><i className='bx bxl-whatsapp'></i> 0732963634</p>
               </div>
             </div>
           </div>
           <div className='col-md-4'>
             <div className='card middle-card'>
-            <div className='card-body'>
+              <div className='card-body'>
                 <h5 className='text-center'>Salesman</h5>
-                <p><i class='bx bxs-user'></i>Sibusiso Bika</p>
-                <p><i class='bx bxl-whatsapp'></i>0834630321</p>
+                <p><i className='bx bxs-user'></i> Sibusiso Bika</p>
+                <p><i className='bx bxl-whatsapp'></i> 0834630321</p>
               </div>
             </div>
           </div>
         </div>
-        
       </div>
-      <div className='container'>
-        <form>
-          <div className=' form-row mt-2 mb-2'>
-            <div className='col-md-6 mb-3'>
-              <label>Name</label>
-              <input className='form-control'></input>
+
+      <div className='container d-flex justify-content-center align-items-center mt-3'>
+        <div className='col-md-6'>
+          <h2 className='text-center'>Leave a comment</h2>
+          <form>
+            <div className='form-row mt-2 mb-2'>
+              <div className='col mb-3'>
+                <label className='mb-2'>Name</label>
+                <input className='form-control' type='text' placeholder='Enter your name' />
+              </div>
             </div>
-          </div>
-          <div className='form-row mb-2 mt-2'>
-            <div className='col-md-6 mb-3'>
-              <label>Email</label>
-              <input className='form-control'></input>
+            <div className='form-row mb-2 mt-2'>
+              <div className='col mb-3'>
+                <label className='mb-2'>Email</label>
+                <input className='form-control' type='email' placeholder='Enter your email' />
+              </div>
             </div>
-          </div>
-          <div className='form-row mb-2 mt-2'>
-            <div className='col-md-6 mb-3'>
-              <label>Subject</label>
-              <input className='form-control'></input>
+            <div className='form-row mb-2 mt-2'>
+              <div className='col mb-3'>
+                <label className='mb-2'>Subject</label>
+                <input className='form-control' type='text' placeholder='Enter subject' />
+              </div>
             </div>
-          </div>
-          <div>
-            <div className='col-md-6 mb-3'>
-            <textarea rows={10} cols={30} className='form-control'></textarea>
+            <div className='form-row mb-2 mt-2'>
+              <div className='col mb-3'>
+                <label>Message</label>
+                <textarea rows={5} className='form-control' placeholder='Enter your message'></textarea>
+              </div>
             </div>
-          </div>
-          <button type='submit' className='btn btn-primary mt-3'>Submit</button>
-        </form>
+            <div className='d-flex justify-content-center'>
+              <button type='submit' className='btn btn-primary mt-3 mb-2'>Submit</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
