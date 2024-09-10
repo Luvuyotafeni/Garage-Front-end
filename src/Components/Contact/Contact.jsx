39b2382ca95f306e0273fea17346aa410c6c9fe7
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-10 13:21:03 +0200

    trying to centre the contact img

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,8 +6,8 @@ const Contact = () => {
   return (
     <div>
       <h2 className='text-center'>Contact Us</h2>
-      <div className='contact_img mt-2 mb-2'>
-        <img src={contact_img} alt='comntact_img' className='contact_img'/>
+      <div className='container contact_img mt-2 mb-2'>
+        <img src={contact_img} alt='contact_img' className='contact_img center-text'/>
       </div>
       <div className='container mt-2'>
         <div className='row'>
@@ -37,17 +37,17 @@ const Contact = () => {
       </div>
       <div className='container'>
         <form>
-          <div className='form-group mt-2 mb-2'>
+          <div className=' form-row mt-2 mb-2'>
             <label>Name</label>
-            <input></input>
+            <input className='form-control'></input>
           </div>
           <div className='form-group mb-2 mt-2'>
             <label>Email</label>
-            <input></input>
+            <input className='form-control'></input>
           </div>
           <div className='form-group mb-2 mt-2'>
             <label>Subject</label>
-            <input></input>
+            <input className='form-control'></input>
           </div>
           <div>
             <textarea rows={10} cols={60}></textarea>
