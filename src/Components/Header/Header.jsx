672ee61f4d32869ce6b5d503008f4487cb97db29
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-11 09:58:42 +0200

    contact page media queries

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,51 +1,52 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "./Header.css"
+import './Header.css';
 
 const Header = () => {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () =>{
-    setIsMenuOpen(isMenuOpen);
+  // Toggle function to open and close the menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggle the state
   };
+
   return (
     <div>
-        <nav className="header">
-          <div className={`nav-links ${isMenuOpen ? 'open': ''}`}>
-            <div className='close-icon' onClick={toggleMenu}>
-              <i className='bx bx-x'></i>
-            </div>
-            <ul >
-                <li>
-                <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/blog">Blog</Link>
-                </li>
-                <li>
-                <Link to="/gallery">Gallery</Link>
-                </li>
-                <li>
-                <Link to="/vehicles"> Vehicles</Link>
-                </li>
-                <li>
-                <Link to="/parts">Parts</Link> 
-                </li>
-                <li>
-                <Link to="/careers">Careers</Link>
-                </li>
-                <li>
-                <Link to="/contact">Contact Us</Link>
-                </li>
-            </ul>
-          </div>
-          <div className='hamburger' onClick={toggleMenu}> 
-            <i className={`bx ${isMenuOpen ? 'bx-x': 'bx-menu'}`}></i>
-          </div>
-        </nav>
-    </div>
-  )
-}
+      <nav className="header">
+        {/* Hamburger Icon - visible only on smaller screens */}
+        <div className="hamburger" onClick={toggleMenu}>
+          <i className={`bx ${isMenuOpen ? 'bx-x' : 'bx-menu'}`}></i>
+        </div>
 
-export default Header
+        {/* Navigation Links */}
+        <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+          <ul>
+            <li>
+              <Link to="/" onClick={toggleMenu}>Home</Link>
+            </li>
+            <li>
+              <Link to="/blog" onClick={toggleMenu}>Blog</Link>
+            </li>
+            <li>
+              <Link to="/gallery" onClick={toggleMenu}>Gallery</Link>
+            </li>
+            <li>
+              <Link to="/vehicles" onClick={toggleMenu}>Vehicles</Link>
+            </li>
+            <li>
+              <Link to="/parts" onClick={toggleMenu}>Parts</Link>
+            </li>
+            <li>
+              <Link to="/careers" onClick={toggleMenu}>Careers</Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={toggleMenu}>Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
