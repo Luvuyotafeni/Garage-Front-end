e773fc130957d98cff862aff7c984f5471e6bdf3
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-11 12:56:24 +0200

    added the vehicles

--- src/Components/Vehicles/Vehicles.jsx
+++ src/Components/Vehicles/Vehicles.jsx
@@ -1,8 +1,158 @@
 import React from 'react'
+import honda from './Assets/honda.jpg'
+import Hyundai from './Assets/HYUNDAI.jpg'
+import Isuzu from './Assets/ISUZU.png'
+import Jaguar from './Assets/JAGUAR.jpg'
+import KIA from './Assets/KIA.png'
+import Mazda from './Assets/MAZDA.png'
+import Mercedes from './Assets/MERCEDES.jpg'
+import MINI from './Assets/MINI.jpg'
+import Mitsubishi from './Assets/MITSUBISHI.png'
+import Nissan from './Assets/MITSUBISHI.png'
+import Opel from './Assets/OPEL.png'
+import Puegeot from './Assets/PEUGEOT.jpg'
+import Renault from './Assets/RENAULT.png'
+import SAAB from './Assets/SAAB.jpg'
+import Tata from './Assets/TATA.jpg'
+import Toyota from './Assets/TOYOTA.jpg'
+import Voklswagen from './Assets/VOLKSWAGEN.jpg'
+import Volvo from './Assets/VOLVO.jpg'
 
 const Vehicles = () => {
   return (
-    <div>Vehicles</div>
+    <div className='container'>
+      <h2>Vehicles</h2>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={honda} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Hyundai} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Isuzu} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Jaguar} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={KIA} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Mazda} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Mercedes} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={MINI} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Mitsubishi} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Nissan} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Opel} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Puegeot} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Renault} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={SAAB} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Tata} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Toyota} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Volvo} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+          <div className='col-md-4'>
+            <div className='card middle-card'>
+              <img src={Voklswagen} />
+              <div className='card-body'></div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
